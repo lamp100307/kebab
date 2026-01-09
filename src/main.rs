45ecf717decd1c914bc6a233df9f0f42a0074a43
dimon <dimongubarev82@gmial.co>
2026-01-09 17:23:13 +0300

    [impl] launch file, automatically install clang if it is not downloaded.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,41 @@
 mod core;
 
+use std::process::{ Command, exit };
+use std::fs::{read_to_string, write, remove_file, File, create_dir_all, rename, remove_dir_all};
+
+use which::which;
+use reqwest::blocking;
+
 use core::lexer::lexer::lex;
 use core::parser::parser::Parser;
 use core::semantic::semantic::SemanticAnalyser;
-use core::llvm::middle_ir::mir_maker::make_middle_ir;
+use core::llvm::middle_ir::mir_maker::{ make_middle_ir, get_dependencies };
 use core::llvm::llvm_ir::generator::LlvmIrGenerator;
 
+fn download_portable_clang() -> Result<(), Box<dyn std::error::Error>> {
+    println!("📦 Downloading Clang...");
+
+    let url = "https://github.com/lamp100307/KebabBack/releases/download/clang/clang.exe";
+    let file_path = "clang.exe";
+
+    let mut response = blocking::get(url)?;
+    let mut file = File::create(file_path)?;
+    std::io::copy(&mut response, &mut file)?;
+
+    println!("✅ Portable Clang ready!");
+    Ok(())
+}
+
 fn main() {
+    if !which("clang").is_ok() || which("clang.exe").is_ok() {
+        match download_portable_clang() {
+            Ok(_) => (),
+            Err(e) => {
+                eprintln!("Failed to download portable Clang: {}", e);
+                exit(1);
+            }
+        }
+    }
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
         eprintln!("Expected at least one argument");
@@ -17,52 +46,98 @@ fn main() {
     let file = &args[1];
     let debug = args.get(2).map(|arg| arg == "debug").unwrap_or(false);
 
-    let contents = std::fs::read_to_string(file).expect("Something went wrong reading the file");
+    let contents = read_to_string(file).expect("Something went wrong reading the file");
 
     let tokens = match lex(&contents) {
         Ok(tokens) => {
             if debug {
-                println!("{:#?}", tokens);
+                println!("Tokens: {:#?}", tokens);
             }
             tokens
         }
         Err(e) => {
             eprintln!("{}", e);
-            std::process::exit(1);
+            exit(1);
         }
     };
 
     let mut parser = Parser::new(tokens);
     let mut ast = match parser.parse() {
-        Ok(ast) => ast,
+        Ok(ast) => {
+            if debug {
+                println!("AST: {:#?}", ast);
+            }
+            ast
+        },
         Err(e) => {
             eprintln!("{}", e);
-            std::process::exit(1);
+            exit(1);
         }
     };
 
-    println!("{}", ast);
-
     ast.optimize();
 
-    println!("{}", ast);
+    if debug {
+        println!("Optimized AST: {:#?}", ast);
+    }
 
     let mut semantic = SemanticAnalyser::new();
 
     match semantic.analyse(&ast) {
-        Ok(()) => println!("Semantic analysis successful!"),
+        Ok(()) => (),
         Err(e) => {
             eprintln!("{}", e);
-            std::process::exit(1);
+            exit(1);
         }
     }
 
+    let dependencies = get_dependencies(&ast);
     let mir = make_middle_ir(ast);
 
-    println!("{:#?}", mir);
+    if debug {
+        println!("Middle IR: {:#?}", mir);
+    }
 
     let mut generator = LlvmIrGenerator::new();
-    let llvm_ir = generator.generate_llvm_ir(mir);
+    let llvm_ir = generator.generate_llvm_ir(mir, dependencies);
 
-    println!("{}", llvm_ir);
+    if debug {
+        println!("LLVM IR: \n{}", llvm_ir);
+    }
+
+    let llvm_file = format!("{}.ll", file);
+    write(&llvm_file, llvm_ir.clone()).unwrap();
+
+    let output_file = format!("{}.exe", file);
+
+    //compiling and running
+    let output = Command::new("clang")
+        .arg(&llvm_file)
+        .arg("-O3")
+        .arg("-o")
+        .arg(output_file.clone())
+        .output()
+        .expect("failed to execute process");
+
+    if !output.stdout.is_empty() {
+        println!("clang output:\n{}", String::from_utf8_lossy(&output.stdout));
+    }
+
+    if !output.stderr.is_empty() {
+        eprintln!("clang errors:\n{}", String::from_utf8_lossy(&output.stderr));
+    }
+
+    if !output.status.success() {
+        eprintln!("clang failed with exit code: {:?}", output.status.code());
+    }
+
+    let output = Command::new(output_file)
+        .output()
+        .expect("failed to execute process");
+
+    if !output.stdout.is_empty() {
+        println!("{}", String::from_utf8_lossy(&output.stdout));
+    }
+
+    remove_file(llvm_file).unwrap();
 }
